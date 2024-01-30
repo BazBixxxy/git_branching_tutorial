35400827445c8c6dd56e3d34a3081218b69c3295
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2024-01-30 12:19:03 +0300

    version 5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let string = "string";
 const stringArray = string.split("");
 stringArray.forEach((value, index) => console.log(value, index));
-console.log("version 4");
+console.log("version 5");
